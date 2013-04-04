af467d933a90f04c480d472a98419c60274d21cd
Rodolphe Quiedeville <rodolphe@quiedeville.org>
2013-04-04 08:41:59 +0200

    Add licence in header

--- wmsosm.erl
+++ wmsosm.erl
@@ -1,5 +1,22 @@
+%%% -*- coding: utf-8 -*-  pylint: disable-msg=R0801
+%%%
+%%% Copyright (c) 2013 Rodolphe Quiédeville <rodolphe@quiedeville.org>
+%%%
+%%%     This program is free software: you can redistribute it and/or modify
+%%%     it under the terms of the GNU General Public License as published by
+%%%     the Free Software Foundation, either version 3 of the License, or
+%%%     (at your option) any later version.
+%%%
+%%%     This program is distributed in the hope that it will be useful,
+%%%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%%     GNU General Public License for more details.
+%%%
+%%%     You should have received a copy of the GNU General Public License
+%%%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%%
 -module(wmsosm).
--export([urlzxy/1,fillall/0]).
+-export([urlzxy/1]).
 
 urlzxy({Pid, Dynvar})->
     {N1,N2,N3} = now(),
