c47567fd79b1e0c282ed642f40975ac00667caf4
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2013-05-17 19:50:53 +0200

    Use constant to be more easily modify

--- tsung.el
+++ tsung.el
@@ -1,6 +1,9 @@
 ;;
 ;; find and open the newest tsung.log files
 ;;
+(defconst tsung-log-dir "~/.tsung/log/")
+(defconst tsung-logfilename "tsung.log")
+
 (defun tsung-open-last-log ()
   "Open the last created tsung.log file"
   (interactive)
@@ -9,12 +12,10 @@
     (car 
      (reverse 
       (sort 
-       (directory-files "~/.tsung/log/" 
+       (directory-files tsung-log-dir 
 			'FULL 
 			(rx bol (repeat 8 digit) "-" (repeat 4 digit) eol)) 'string-lessp)
       ))
-    "/tsung.log")
+    "/" tsung-logfilename)
    )
   )
-
-
