fda0f9496336426c8c94a2b13052617cafbcdf38
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2013-09-18 19:30:22 +0200

    Add test on num2deg

--- slippymap_tests.erl
+++ slippymap_tests.erl
@@ -11,4 +11,14 @@
 -module(slippymap_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-compile(export_all).
+
 test()-> ok.
+
+
+num2deg_test()->
+    ?assertEqual({-135.0,79.17133464081945}, slippymap:num2deg(4,4,5)).
+
+num2deg2_test()->
+    ?assertEqual({-180.0, 85.05112877980659}, slippymap:num2deg(0,0,5)).
+
