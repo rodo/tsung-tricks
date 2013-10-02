fe69b505659a08bff7236c789429796d42e978a4
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2013-10-02 15:13:32 +0200

    Clean code

--- jsonurls_tests.erl
+++ jsonurls_tests.erl
@@ -8,22 +8,22 @@
 
 %% Tests
 url_test()->
-    Data = {struct,[{<<"url">>,"foo1"}]},
+    Data = {struct, [{<<"url">>, "foo1"}]},
     ?assertEqual("foo1", jsonurls:url(Data)).
 
 url_binary_test()->
-    Data = {struct,[{<<"url">>,<<"foo1">>}]},
+    Data = {struct, [{<<"url">>, <<"foo1">>}]},
     ?assertEqual("foo1", jsonurls:url(Data)).
 
 fillurls_test()->
-    Data = [{struct,[{<<"url">>,"foo1"}]}, {struct,[{<<"url">>,"foo2"}]}],
+    Data = [{struct,[{<<"url">>, "foo1"}]}, {struct,[{<<"url">>,"foo2"}]}],
     Assert = ["foo1", "foo2"],
     ?assertEqual(Assert, jsonurls:fillurls(Data)).
 
 decode_test()->
     Data = ts_dynvars:new(jsonurls, "[{\"url\":\"/foobar/index?page=0\"},{\"url\":\"/foo?p=1\"}]"),
-    Assert = [{struct,[{<<"url">>,<<"/foobar/index?page=0">>}]},
-              {struct,[{<<"url">>,<<"/foo?p=1">>}]}],
+    Assert = [{struct,[{<<"url">>, <<"/foobar/index?page=0">>}]},
+              {struct,[{<<"url">>, <<"/foo?p=1">>}]}],
     ?assertEqual(Assert, jsonurls:decode(Data)).
 
 get_url_test()->
