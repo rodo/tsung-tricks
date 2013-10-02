93a5bbed5065fb3a6efee84b2e48c6258e0952a5
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2013-10-02 15:17:12 +0200

    Clean code

--- slippymap.erl
+++ slippymap.erl
@@ -17,22 +17,22 @@
 % @spec deg2num(Lat::float(), Lon:: float(), Zoom::integer()) -> {integer(), integer()}
 %
 deg2num(Lat,Lon,Zoom)->
-    X=math:pow(2, Zoom) * ((Lon + 180) / 360),
-    Sec=1/math:cos(deg2rad(Lat)),
-    R = math:log(math:tan(deg2rad(Lat)) + Sec)/math:pi(),
-    Y=math:pow(2, Zoom) * (1 - R) / 2,
-    {round(X),round(Y)}.
+    X = math:pow(2, Zoom) * ((Lon + 180) / 360),
+    Sec = 1 / math:cos(deg2rad(Lat)),
+    R = math:log(math:tan(deg2rad(Lat)) + Sec) / math:pi(),
+    Y = math:pow(2, Zoom) * (1 - R) / 2,
+    {round(X), round(Y)}.
 
 % @doc convert tile numbers to geometric coordinate
 %
 % @spec num2deg(X::integer(), Y::integer(), Zoom::integer()) -> {float(), float()}
 %
 num2deg(X,Y,Zoom)->
-    N=math:pow(2, Zoom),
-    Lon=X/N*360-180,
-    Lat_rad=math:atan(math:sinh(math:pi()*(1-2*Y/N))),
-    Lat=Lat_rad*180/math:pi(),
-    {Lon,Lat}.
+    N = math:pow(2, Zoom),
+    Lon = X / N * 360 - 180,
+    Lat_rad = math:atan(math:sinh(math:pi() * (1 - 2 * Y / N))),
+    Lat = Lat_rad * 180 / math:pi(),
+    {Lon, Lat}.
 
 deg2rad(C)->
-    C*math:pi()/180.
+    C * math:pi() / 180.
